cfa0b7bf2767e45abdafc21dbe40224c8eff96f5
diohcospefire <simaessilva@gmail.com>
2024-12-14 18:37:58 +0000

    menu changes
    -added easy level scenes
    -added medium level scenes
    -added hard level scenes
    -imported 2 new pictures to the project
    -the 2 new pictures sliced into separated puzzle pieces

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -5,10 +5,43 @@ using UnityEngine.SceneManagement;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    public void PlayGame()
+    public void PlayEasy1()
     {
-        SceneManager.LoadScene("Puzzle");
+        SceneManager.LoadScene("Easy1");
     }
+    public void PlayEasy2()
+    {
+        SceneManager.LoadScene("Easy2");
+    }
+    public void PlayEasy3()
+    {
+        SceneManager.LoadScene("Easy3");
+    }
+    public void PlayMedium1()
+    {
+        SceneManager.LoadScene("Medium1");
+    }
+    public void PlayMedium2()
+    {
+        SceneManager.LoadScene("Medium2");
+    }
+    public void PlayMedium3()
+    {
+        SceneManager.LoadScene("Medium3");
+    }
+    public void PlayHard1()
+    {
+        SceneManager.LoadScene("Hard1");
+    }
+    public void PlayHard2()
+    {
+        SceneManager.LoadScene("Hard2");
+    }
+    public void PlayHard3()
+    {
+        SceneManager.LoadScene("Hard3");
+    }
+
 
     public void QuitGame()
     {
